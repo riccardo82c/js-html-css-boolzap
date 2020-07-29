2df548e367e5559193aab5c92d417964ba1551c3
Riccardo Colombo <rikyregna@gmail.com>
2020-07-29 16:16:28 +0200

    i hate clones

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
+// $(function () {
+
+
+
+
+
+// $('#input-mes').keydown(inserisci);
+
+// 	function inserisci(e) {
+// 		if (e.which == 13 || e.keyCode == 13) {
+
+// 			var testo = $(this).val();
+// 			if (testo != '') {
+// 				console.log(testo);
+// 				$(this).val('')
+// 				// clono tutto l'elemento bubble
+// 				var elemento = $('.template .bubble').clone();
+// 				// modifico l'elemento NUOVO in memoria
+// 				elemento.addClass('send');
+// 				elemento.find('.bubble-text').append(testo);
+// 				// lo appendo nel DOM
+// 				$('.chat').append(elemento);
+// 				/* template.append(testo); */
+// 			} else {
+// 				console.log('riga vuota');
+// 			}
+// 		}
+// 	}
+// });
+
+
 $(function () {
-
-	var template = $('#template .bubble').clone();
-	console.log(template);
-
 
 	$('#input-mes').keydown(inserisci);
 
@@ -10,46 +37,36 @@ $(function () {
 		if (e.which == 13 || e.keyCode == 13) {
 
 			var testo = $(this).val();
-			$(this).val('')
-			console.log(testo);
+
+
+			var data = new Date();
+			var ora = data.getHours() + ":" + data.getMinutes();
+
+			$('.chat').append(`
+					<div class="bubble send">
+						<p class='bubble-text'>${testo}</p>
+						<div class="bubble-time">${ora}</div>
+					</div>
+				</div>`);
+
+
+			setTimeout(risposta, 3000);
+
+
+			function risposta() {
+				var testo = 'Buonanotte';
+				var data = new Date();
+				var ora = data.getHours() + ":" + data.getMinutes();
+
+				$('.chat').append(`
+					<div class="bubble received">
+						<p class='bubble-text'>${testo}</p>
+						<div class="bubble-time">${ora}</div>
+					</div>
+				</div>`);
+
+			}
 		}
 	}
+
 });
-
-
-
-
-
-/* $(function () {
-
-	var testo = 'prova';
-	var data = new Date();
-	var ora = data.getHours() + ":" + data.getMinutes();
-
-	$('.chat').append(`<div class="message send">
-					<div class="bubble">
-						<p>${testo}</p>
-						<div class="bubble-time">${ora}</div>
-					</div>
-				</div>`);
-
-
-	setTimeout(risposta, 3000);
-
-
-	function risposta() {
-		var testo = 'Buonanotte';
-		var data = new Date();
-		var ora = data.getHours() + ":" + data.getMinutes();
-
-		$('.chat').append(`<div class="message received">
-					<div class="bubble">
-						<p>${testo}</p>
-						<div class="bubble-time">${ora}</div>
-					</div>
-				</div>`);
-
-	}
-
-
-}); */
